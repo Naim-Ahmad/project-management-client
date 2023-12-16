0880777d87b09b28f29bfcd7f9bc972f5618444b
Naim Ahmad <naimahmad201103@gmail.com>
2023-12-16 10:47:53 +0600

    sticky fixed dashboard sidebar

--- src/layouts/DashbordLayout.jsx
+++ src/layouts/DashbordLayout.jsx
@@ -1,21 +1,21 @@
-import { Outlet } from "react-router-dom";
-import { Sidebar } from "./sidebar/Sidebar";
-import DashboardNav from "./dashboardNav/DashboardNav";
 import { useSelector } from "react-redux";
+import { Outlet } from "react-router-dom";
+import DashboardNav from "./dashboardNav/DashboardNav";
+import { Sidebar } from "./sidebar/Sidebar";
 
 const DashbordLayout = () => {
   const { showNav } = useSelector((state) => state.dashboard);
   const { loading } = useSelector((state) => state.auth);
   if (loading) return <div>Auth checking...</div>;
   return (
-    <section className="flex h-screen">
+    <section className=" flex ">
       {showNav && (
-        <div className="max-w-xs  absolute left-0 top-16 z-50 transition-all duration-300 lg:static">
+        <div className="sticky top-0 h-[90vh] transition-all duration-300">
           <Sidebar />
         </div>
       )}
 
-      <div className="bg-[#EEF1F3] flex-grow transition h-screen w-full">
+      <div className="bg-[#EEF1F3] flex-grow transition w-full">
         <DashboardNav />
         <div className="p-4">
           <Outlet />
