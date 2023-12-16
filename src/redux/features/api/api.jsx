adf67de4fa1bc888920f1d0ea6bc1975b7ab1018
Naim Ahmad <naimahmad201103@gmail.com>
2023-12-16 09:46:33 +0600

    added origin in rtk query

--- src/redux/features/api/api.jsx
+++ src/redux/features/api/api.jsx
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BASE_URL,
+    baseUrl: "https://project-management-server-seven.vercel.app",
     prepareHeaders: (headers) => {
       headers.set("content-type", "application/json");
       return headers;
