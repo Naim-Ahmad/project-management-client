346259dcb7575432d41d804338eb470346bf3dde
Tanjil Hossain <tanjilhossain845@gmail.com>
2023-12-17 14:51:33 +0600

    fixed confect issue

--- src/redux/features/api/api.jsx
+++ src/redux/features/api/api.jsx
@@ -5,12 +5,7 @@ const env = import.meta.env
 const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-<<<<<<< HEAD
-    // baseUrl: "https://project-management-server-seven.vercel.app",
-    baseUrl: "http://localhost:3000",
-=======
     baseUrl: env.VITE_BASE_URL,
->>>>>>> 4d13769af67ac6025f6296d73373b5a98156cf5e
     prepareHeaders: (headers) => {
       headers.set("content-type", "application/json");
       return headers;
